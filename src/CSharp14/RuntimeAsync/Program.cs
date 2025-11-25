b36a94f5292d626de91672529723b9893257490e
Andrea Ceroni <andrea.ceroni@elfo.net>
2025-11-25 07:06:18 +0100

    update

---
+++ src/CSharp14/RuntimeAsync/Program.cs
@@ -0,0 +1,22 @@
+﻿// https://github.com/dotnet/runtime/blob/main/docs/design/specs/runtime-async.md
+// https://github.com/dotnet/runtime/issues/109632
+// https://dev.to/iron-software/c-asyncawait-in-net-10-the-complete-technical-guide-for-2025-1cii
+using System.Runtime.CompilerServices;
+
+async Task<int> DoThingAsync()
+{
+    await Task.Yield();
+    return 42;
+}
+
+/*
+[MethodImpl(MethodImplOptions.Async)]
+Task<string> SayCiao()
+{
+#pragma warning disable SYSLIB5007
+    AsyncHelpers.Await(Task.Delay(TimeSpan.FromSeconds(1)));
+#pragma warning restore SYSLIB5007
+    
+    return "ciao";
+}
+*/
---
+++ src/CSharp14/Union/Program.cs
@@ -0,0 +1,55 @@
+﻿// https://github.com/dotnet/csharplang/blob/main/meetings/working-groups/discriminated-unions/union-proposals-overview.md
+// https://github.com/dotnet/csharplang/blob/main/proposals/unions.md
+
+Bike bike = new Mtb("Spark");
+
+#region var model = bike switch
+var model = bike.Value switch
+#endregion
+{
+    Mtb mtb => mtb.Model,
+    Bdc bdc => bdc.Model,
+    Gravel gravel => gravel.Model,
+    //_ => "Unknown bike type"
+};
+
+#region public union Bike(Bike1, Bike2, Bike3)
+public partial record struct Bike : IUnion
+{
+    public Bike(Mtb value) => Value = value;
+    public Bike(Bdc value) => Value = value;
+    public Bike(Gravel value) => Value = value;
+        
+    public object? Value { get; }
+        
+    public static implicit operator Bike(Mtb value) => new(value);
+    public static implicit operator Bike(Bdc value) => new(value);
+    public static implicit operator Bike(Gravel value) => new(value);
+}
+#endregion
+
+#region IUnion interface
+public interface IUnion
+{
+    object? Value { get; }
+}
+#endregion
+
+#region cases
+
+public class Mtb(string model)
+{
+    public string Model { get; set; } = model;
+}
+    
+public class Bdc(string model)
+{
+    public string Model { get; set; } = model;
+}
+
+public class Gravel(string model)
+{
+    public string Model { get; set; } = model;
+}
+
+#endregion
